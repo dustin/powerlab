d52d252cea8934b275fe136e85527f4e0bd26052
Dustin Sallings <dustin@spy.net>
2015-06-14 00:35:51 -0700

    Fixed stupidity in logging raw data

--- logger.go
+++ logger.go
@@ -16,18 +16,13 @@ type LogEntry struct {
 
 // Log this status to a stream.
 func (s *Status) Log(t time.Time, w io.Writer) error {
-	j, err := json.Marshal(s)
-	if err != nil {
-		return err
-	}
-
-	j2, err := json.Marshal(LogEntry{t, j, s})
+	j, err := json.Marshal(LogEntry{t, (*s)[:], s})
 	if err != nil {
 		return err
 	}
 
 	buf := &bytes.Buffer{}
-	json.Indent(buf, j2, "", "  ")
+	json.Indent(buf, j, "", "  ")
 	if _, err := w.Write(buf.Bytes()); err != nil {
 		return err
 	}
