0703a32f8768cc9e29ab10e962e87a4f1e6e24ea
Dustin Sallings <dustin@spy.net>
2015-06-15 23:09:15 -0700

    Use s/ /-/ for mode selectors

--- powerserver/static/powerserver.js
+++ powerserver/static/powerserver.js
@@ -47,8 +47,10 @@ function updateStatus(dees) {
     var d = last.ST;
     lastStatus = last.TS;
 
-    if (d.mode == 'charging' || d.mode == 'discharging' || d.mode == 'pack cool down') {
-        d3.select("#mode").text(d.mode + " " + sp(d) + " " + d.avg_cell.toFixed(1) + "%");
+    var mode = d.mode.replace(' ', '-');
+
+    if (mode == 'charging' || mode == 'discharging' || mode == 'pack cool down') {
+        d3.select("#mode").text(mode + " " + sp(d) + " " + d.avg_cell.toFixed(1) + "%");
         if (d.charge_complete) {
             $("#mode").addClass('complete');
         } else {
@@ -56,18 +58,18 @@ function updateStatus(dees) {
         }
         $(".chart").show();
     } else {
-        d3.select("#mode").text(d.mode);
+        d3.select("#mode").text(mode);
         $(".chart").hide();
     }
-    d3.select("#" + d.mode + "-chemistry").text(d.chemistry);
-    d3.select("#" + d.mode + "-amps").text(d.fast_amps.toFixed(2));
-    d3.select("#" + d.mode + "-mah").text(d.mah_in);
-    d3.select("#" + d.mode + "-mahout").text(d.mah_out);
-    d3.select("#" + d.mode + "-volts").text(farray(d.voltage.splice(0, d.detected_cell_count)));
-    d3.select("#" + d.mode + "-ir").text(farray(d.ir.splice(0, d.detected_cell_count)));
-    d3.select("#" + d.mode + "-time").text(d.charge_time);
+    d3.select("#" + mode + "-chemistry").text(d.chemistry);
+    d3.select("#" + mode + "-amps").text(d.fast_amps.toFixed(2));
+    d3.select("#" + mode + "-mah").text(d.mah_in);
+    d3.select("#" + mode + "-mahout").text(d.mah_out);
+    d3.select("#" + mode + "-volts").text(farray(d.voltage.splice(0, d.detected_cell_count)));
+    d3.select("#" + mode + "-ir").text(farray(d.ir.splice(0, d.detected_cell_count)));
+    d3.select("#" + mode + "-time").text(d.charge_time);
 
-    $("#" + d.mode).show();
+    $("#" + mode).show();
 
     batterygauge.redraw(d.avg_cell);
     cellchart();
