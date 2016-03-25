d3a38ad11a1b25572141bb74f682c7d357c46fb0
Dustin Sallings <dustin@spy.net>
2016-03-24 23:57:27 -0700

    Cheaper encoding of JSON slices

--- powerserver/httputil.go
+++ powerserver/httputil.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"reflect"
 	"strings"
 )
 
@@ -39,11 +40,43 @@ func (g *gzippingWriter) Close() error {
 	return nil
 }
 
+func encodeJSONSlice(w io.Writer, thing interface{}) error {
+	w.Write([]byte{'['})
+	defer w.Write([]byte{']'})
+
+	v := reflect.ValueOf(thing)
+	for i := 0; i < v.Len(); i++ {
+		item := v.Index(i).Interface()
+		var j []byte
+		var err error
+		if it, ok := item.(json.Marshaler); ok {
+			j, err = it.MarshalJSON()
+		} else {
+			j, err = json.Marshal(item)
+		}
+		if err != nil {
+			return err
+		}
+		if _, err := w.Write(j); err != nil {
+			return err
+		}
+		if i+1 < v.Len() {
+			w.Write([]byte{','})
+		}
+	}
+
+	return nil
+}
+
 func serveJSON(w http.ResponseWriter, req *http.Request, thing interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 
 	out := newGzippingWriter(w, req)
 	defer out.Close()
+
+	if reflect.TypeOf(thing).Kind() == reflect.Slice {
+		return encodeJSONSlice(out, thing)
+	}
 
 	return json.NewEncoder(out).Encode(thing)
 }
