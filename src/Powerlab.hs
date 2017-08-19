3ced2ba248412debb71875cad90dfa60f1d64286
dustin <dustin@spy.net>
2017-08-19 21:15:31 +0000

    Working crc16 with better symbols.

--- src/Powerlab.hs
+++ src/Powerlab.hs
@@ -6,17 +6,19 @@ import Data.Bits
 import Data.Word
 import qualified Data.ByteString as B
 
-to_w16 :: Enum a => a -> Word16
-to_w16 = (toEnum . fromEnum)
+to_w16 = (toEnum . fromEnum) :: Enum a => a -> Word16
+to_w32 = (toEnum . fromEnum) :: Enum a => a -> Word32
 
-to_w32 =( toEnum . fromEnum) :: Word8 -> Word32
+-- logical symbols since haskell doesn't have them.
+(≫) = shiftR
+(⊕) = xor
 
+-- Not yet needed, but since I was typing...
+-- (≪) = shiftL
+-- (∧) = (.&.)
+-- (∥) = (.|.)
 
 crc16 :: B.ByteString -> Word16
-crc16 x = to_w16 $ B.foldl stupid_bit_stuff (4742::Word32) x
-
-stupid_bit_stuff n b =
-  foldl (\a x ->
-            let t = x `xor` a in
-              (a `shiftR` 1) `xor` if (t `shiftR` 1) == (t-1) `shiftR` 1 then 33800 else 0
-        ) n [(to_w32 b) `shiftR` x | x <- [0..7]]
+crc16 x = let
+  perbyte n b = foldl (\a x -> (a ≫ 1) ⊕ if odd (x ⊕ a) then 33800 else 0) n [(to_w32 b) ≫ x | x <- [0..7]] in
+    to_w16 $ B.foldl perbyte 4742 x
