4f144c49b6b4ed0916afeb2076a1da7f3ab82bfd
dustin <dustin@spy.net>
2017-08-19 21:17:40 +0000

    Slightly easier to read crc16 with the per bit and per byte stuff separated

--- src/Powerlab.hs
+++ src/Powerlab.hs
@@ -20,5 +20,6 @@ to_w32 = (toEnum . fromEnum) :: Enum a => a -> Word32
 
 crc16 :: B.ByteString -> Word16
 crc16 x = let
-  perbyte n b = foldl (\a x -> (a ≫ 1) ⊕ if odd (x ⊕ a) then 33800 else 0) n [(to_w32 b) ≫ x | x <- [0..7]] in
+  perbit a x = (a ≫ 1) ⊕ if odd (x ⊕ a) then 33800 else 0
+  perbyte n b = foldl perbit n [(to_w32 b) ≫ x | x <- [0..7]] in
     to_w16 $ B.foldl perbyte 4742 x
