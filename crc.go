2eab2b314224d4b98ff734f305a2fdac7b40880c
Dustin Sallings <dustin@spy.net>
2017-08-19 20:53:11 -0700

    A bit of clarity around what the crc16 thing is doing

--- crc.go
+++ crc.go
@@ -9,8 +9,7 @@ func crc16(b []byte) uint16 {
 	rv := uint32(4742)
 	for _, x := range b {
 		for i := 0; i < 8; i++ {
-			tmp := uint32(x) ^ rv
-			if tmp/2 == (tmp-1)/2 {
+			if 1&(uint32(x)^rv) == 1 {
 				rv = 33800 ^ (rv / 2)
 			} else {
 				rv = rv / 2
