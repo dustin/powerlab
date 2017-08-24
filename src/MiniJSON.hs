83982ecc81ab551c44215acd1951eaaf76fe4dd8
dustin <dustin@spy.net>
2017-08-24 15:54:25 +0000

    Make +++ a bytestring append operator

--- src/MiniJSON.hs
+++ src/MiniJSON.hs
@@ -13,6 +13,8 @@ import qualified Data.ByteString.Lazy.Char8 as BC
 e :: String -> B.ByteString
 e = BC.pack
 
+(+++) = B.append
+
 esc :: String -> B.ByteString
 esc [] = B.empty
 esc (x:xs)
@@ -23,9 +25,9 @@ esc (x:xs)
   | x == '\f' = b 'f'  xs
   | x == '\t' = b 't'  xs
   | x == '"'  = b '"'  xs
-  | d < 0x1f  = (bs '\\') `B.append` (bs 'u') `B.append` leftpad (showIntAtBase 16 intToDigit d "") `B.append` (esc xs)
-  | otherwise = (bs x) `B.append` esc xs
-  where b c xs = (bs '\\') `B.append` (bs c) `B.append` esc xs
+  | d < 0x1f  = (bs '\\') +++ (bs 'u') +++ leftpad (showIntAtBase 16 intToDigit d "") +++ (esc xs)
+  | otherwise = (bs x) +++ esc xs
+  where b c xs = (bs '\\') +++ (bs c) +++ esc xs
         leftpad :: String -> B.ByteString
         leftpad s
           | length s == 4 = e s
@@ -38,17 +40,17 @@ class ToJSON a where
 
   toJSONList :: [a] -> B.ByteString
 
-  toJSONList x = (e "[") `B.append` (B.intercalate (e ", ") $ map toJSON x) `B.append` (e "]")
+  toJSONList x = (e "[") +++ (B.intercalate (e ", ") $ map toJSON x) +++ (e "]")
 
 instance ToJSON Char where
   toJSON x = toJSONList [x]
-  toJSONList s = (BC.singleton '"') `B.append` (esc s) `B.append` (BC.singleton '"')
+  toJSONList s = (BC.singleton '"') +++ (esc s) +++ (BC.singleton '"')
 
 instance ToJSON UTCTime where
   toJSON = toJSON . formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
 
 instance ToJSON Text where
-  toJSON s = (e "\"") `B.append` (e $ unpack s) `B.append` (e "\"")
+  toJSON s = (e "\"") +++ (e $ unpack s) +++ (e "\"")
 
 instance ToJSON Double where
   toJSON d = e $ if isNaN d then "null" else show d
@@ -70,10 +72,10 @@ instance ToJSON t => ToJSON (Maybe t) where
 data JSONPair = JSONPair B.ByteString B.ByteString
 
 instance ToJSON JSONPair where
-  toJSON (JSONPair l r) = l `B.append` (e ": ") `B.append` r
+  toJSON (JSONPair l r) = l +++ (e ": ") +++ r
 
 object :: [JSONPair] -> B.ByteString
-object l = (e "{") `B.append` (B.intercalate (e ", ") $ map toJSON l) `B.append` (e "}")
+object l = (e "{") +++ (B.intercalate (e ", ") $ map toJSON l) +++ (e "}")
 
 (.=) :: ToJSON t => String -> t -> JSONPair
 (.=) k v = JSONPair (toJSON k) (toJSON v)
