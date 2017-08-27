ad4c9335dc1b36c72ce0607494b5de8ca9782a85
dustin <dustin@spy.net>
2017-08-27 05:00:59 +0000

    Make it clear when the updater thread terminates

--- app/Main.hs
+++ app/Main.hs
@@ -108,7 +108,7 @@ main = do
   let statApp = staticApp $ (defaultWebAppSettings (optStatic opts)) {ssIndices = [unsafeToPiece "index.html"]}
 
   tv <- atomically newState
-  forkFinally (updater tv (optSerial opts)) (\x -> error $ show x)
+  forkFinally (updater tv (optSerial opts)) (\x -> error $ "updater fatality: " ++ (show x))
 
   putStrLn $ "http://localhost:" ++ (show $ optPort opts)
   run (optPort opts) $ app statApp tv
