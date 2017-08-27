4d12f6502f8c46a924e09007a05c796c3398d65c
dustin <dustin@spy.net>
2017-08-27 03:10:55 +0000

    Look for the matching response before proceeding

--- src/Powerlab/Serial.hs
+++ src/Powerlab/Serial.hs
@@ -5,6 +5,7 @@ module Powerlab.Serial (
 import Powerlab
 import qualified Powerlab.Status as St
 import Control.Concurrent
+import Control.Monad
 
 import System.Hardware.Serialport
 import qualified Data.ByteString.Lazy as LB
@@ -14,16 +15,25 @@ type MessageHandler = St.Status -> IO ()
 
 statusReq = B.pack ['R', 'a', 'm', '\0']
 
+read_match :: Int -> SerialPort -> String -> IO Bool
+read_match 0 _ _ = return False
+read_match _ s [] = return True
+read_match ttl s l@(x:xs) = do
+  c <- recv s 1
+  if (B.head c) == x then read_match (pred ttl) s xs else read_match (pred ttl) s l
+
 readStatus :: SerialPort -> MessageHandler -> IO ()
 readStatus s h = do
+  matched <- read_match 8 s (B.unpack statusReq)
+  when matched $ return ()
+
   d <- recv s (fromEnum St.statusLen)
-  h $ St.parse (LB.fromStrict d)
+  h $ St.parse (LB.fromStrict $ statusReq `B.append` d)
 
 loop :: MessageHandler -> SerialPort -> IO ()
 loop h s = do
   send s $ statusReq
-  cmd <- recv s 4
-  if cmd == statusReq then ( readStatus s h ) else (error $ "misread: " ++ (show cmd))
+  readStatus s h
   threadDelay 1000000
   loop h s
 
