8db88d02aa2d603869f4e340251de682af259ad8
dustin <dustin@spy.net>
2017-10-27 01:07:44 +0000

    Increase test count to 1000

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ import StatusTest
 import MiniJSONTest
 
 import Test.Framework.Runners.Options
+import Test.Framework.Options (TestOptions'(..))
 import Test.Framework (defaultMainWithOpts, interpretArgsOrExit)
 import System.Environment (getArgs)
 
@@ -10,4 +11,5 @@ import System.Environment (getArgs)
 main :: IO ()
 main = do opts <- interpretArgsOrExit =<< getArgs
           defaultMainWithOpts (CRCTest.tests ++ StatusTest.tests ++ MiniJSONTest.tests)
-            opts { ropt_hide_successes = Just True }
+            opts { ropt_hide_successes = Just True,
+                   ropt_test_options = Just $ mempty { topt_maximum_generated_tests = Just 1000 }}
