04b82e88342931659565612e37be3372747bc6a8
Dustin Sallings <dustin@spy.net>
2017-11-22 21:07:03 -0800

    Don't need all these helper functions
    OverloadedStrings and a Show -> ByteString does most of it.

--- src/MiniJSON.hs
+++ src/MiniJSON.hs
@@ -1,15 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module MiniJSON (ToJSON(..), object, encode, (.=)) where
 
 import Data.Semigroup ((<>))
 import Data.Text (Text, unpack)
-import Data.Time
+import Data.Time (UTCTime, formatTime, defaultTimeLocale)
+import Data.String (fromString)
 import Numeric (showHex)
 
 import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BC
-
-e :: String -> B.ByteString
-e = BC.pack
 
 esc :: String -> B.ByteString
 esc [] = B.empty
@@ -26,7 +26,7 @@ esc (x:xs)
   where b c = bs '\\' <> bs c <> esc xs
         leftpad :: String -> B.ByteString
         leftpad s
-          | length s == 4 = e s
+          | length s == 4 = fromString s
           | otherwise = leftpad $ '0' : s
         bs = BC.singleton
         d = fromEnum x
@@ -36,7 +36,7 @@ class ToJSON a where
 
   toJSONList :: [a] -> B.ByteString
 
-  toJSONList x = e "[" <> B.intercalate (e ", ") (map toJSON x) <> e "]"
+  toJSONList x = "[" <> B.intercalate ", " (map toJSON x) <> "]"
 
 instance ToJSON Char where
   toJSON x = toJSONList [x]
@@ -46,40 +46,43 @@ instance ToJSON UTCTime where
   toJSON = toJSON . formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S"
 
 instance ToJSON Text where
-  toJSON s = e "\"" <> esc (unpack s) <> e "\""
+  toJSON s = "\"" <> esc (unpack s) <> "\""
+
+bcshow :: Show s => s -> B.ByteString
+bcshow = fromString.show
 
 instance ToJSON Double where
   toJSON d
     | isNaN d = nil
     | d == 1/0 = nil
     | d == -1/0 = nil
-    | otherwise = (e.show) d
-    where nil = e "null"
+    | otherwise = bcshow d
+    where nil = "null"
 
 instance ToJSON Int where
-  toJSON = e . show
+  toJSON = bcshow
 
 instance ToJSON Integer where
-  toJSON = e . show
+  toJSON = bcshow
 
 instance ToJSON a => ToJSON [a] where
   toJSON = toJSONList
 
 instance ToJSON Bool where
-  toJSON True = e "true"
-  toJSON False = e "false"
+  toJSON True = "true"
+  toJSON False = "false"
 
 instance ToJSON t => ToJSON (Maybe t) where
-  toJSON Nothing = e "null"
+  toJSON Nothing = "null"
   toJSON (Just t) = toJSON t
 
 data JSONPair = JSONPair B.ByteString B.ByteString
 
 instance ToJSON JSONPair where
-  toJSON (JSONPair l r) = l <> e ": " <> r
+  toJSON (JSONPair l r) = l <> ": " <> r
 
 object :: [JSONPair] -> B.ByteString
-object l = e "{" <> B.intercalate (e ", ") (map toJSON l) <> e "}"
+object l = "{" <> B.intercalate ", " (map toJSON l) <> "}"
 
 (.=) :: ToJSON t => String -> t -> JSONPair
 (.=) k v = JSONPair (toJSON k) (toJSON v)
