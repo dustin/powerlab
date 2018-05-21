d3f84e81ce516d5ca8c100caafcd1a4ab6cf2d1e
Dustin Sallings <dustin@spy.net>
2018-05-20 22:03:14 -0700

    Remove head (partial function blowing up)

--- src/Powerlab/Serial.hs
+++ src/Powerlab/Serial.hs
@@ -5,6 +5,7 @@ module Powerlab.Serial (
 import qualified Powerlab.Status as St
 import Control.Concurrent
 import Control.Monad
+import Data.Semigroup ((<>))
 
 import System.Hardware.Serialport
 import qualified Data.ByteString.Lazy as LB
@@ -16,25 +17,29 @@ statusReq :: B.ByteString
 statusReq = B.pack ['R', 'a', 'm', '\0']
 
 readMatch :: Int -> SerialPort -> String -> IO Bool
-readMatch 0 _ _ = return False
-readMatch _ _ [] = return True
+readMatch 0 _ _ = pure False
+readMatch _ _ [] = pure True
 readMatch ttl s l@(x:xs) = do
   c <- recv s 1
-  if B.head c == x then readMatch (pred ttl) s xs else readMatch (pred ttl) s l
+  case B.unpack c of
+    [x'] -> if x' == x then readMatch (pred ttl) s xs else next
+    _ -> next
+
+  where next = readMatch (pred ttl) s l
 
 readFull :: SerialPort -> Int -> IO B.ByteString
-readFull _ 0 = return B.empty
+readFull _ 0 = pure B.empty
 readFull s l = do
   r <- recv s l
   rest <- readFull s $ l - B.length r
-  return $ r `B.append` rest
+  pure $ r <> rest
 
 readStatus :: SerialPort -> MessageHandler -> IO (Either String St.Status)
 readStatus s _ = do
   matched <- readMatch 8 s (B.unpack statusReq)
   d <- if matched then readFull s (fromEnum St.statusLen) else return B.empty
   let pkt = LB.fromStrict $ statusReq `B.append` d
-  return $! St.parse pkt
+  pure $! St.parse pkt
 
 loop :: MessageHandler -> SerialPort -> IO ()
 loop h s = forever $ do
